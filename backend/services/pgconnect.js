027f4c2068b40f7ec7ec83fde64416e7989e9007
Wallace <wallacegomes6877@gmail.com>
2025-10-27 12:03:03 -0300

    feat:connection to db

---
+++ backend/services/pgconnect.js
@@ -0,0 +1,15 @@
+import pg, { Pool } from 'pg';
+import 'dotenv/config'
+
+export async function ConnectToDbAndReturnPoll()
+{
+    const client = new pg.Pool({
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT
+    })
+
+    return client
+}
