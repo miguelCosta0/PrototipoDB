4bc9631138482ea03ba2a5a2dad7ffdb0569bd3f
Wallace <wallacegomes6877@gmail.com>
2025-10-28 17:34:36 -0300

    feat: pg connection is a singleton

--- backend/services/pgconnect.js
+++ backend/services/pgconnect.js
@@ -1,15 +1,19 @@
 import pg, { Pool } from 'pg';
 import 'dotenv/config'
 
+var pool;
+
 export function ConnectToDbAndReturnPoll()
 {
-    const client = new pg.Pool({
+    if (pool != undefined) return pool
+    pool = new pg.Pool({
         host: process.env.DB_HOST || "localhost",
         user: process.env.DB_USER || "postgres",
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        options: `-c search_path=ecommerce,public`
     })
 
-    return client
+    return pool
 }
