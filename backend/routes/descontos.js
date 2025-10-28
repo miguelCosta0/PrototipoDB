cb7949db914e00e6c97cdeff04cdf97bbdf630b6
Wallace <wallacegomes6877@gmail.com>
2025-10-28 19:19:06 -0300

    feat: desconto post endpoint

--- backend/routes/descontos.js
+++ backend/routes/descontos.js
@@ -26,4 +26,27 @@ router.get('/:id', async function(req, res, next) {
   res.send(prod.rows)
 });
 
+router.post('/', async (req, res) => {
+	let dataInicio = req.body.data_inicio
+	let dataFim = req.body.data_fim
+	let porcentagem = req.body.porcentagem
+
+	if(dataInicio == undefined || dataFim == undefined || porcentagem == undefined)
+	{
+		res.status(400).send({error: "Todos os campos sao obrigatorios, leia a documentacao"})
+	}
+
+	if(isNaN(porcentagem) || porcentagem > 1 || porcentagem <= 0)
+	{
+		res.status(400).send({error: "A porcentagem de desconto deve ser um valor numerico entre 0 e 1, leia a documentacao"})
+	}
+
+	try {
+		let descontoInserted = await dbPool.query("INSERT INTO desconto(data_inicio, data_fim, porcentagem) VALUES ($1,$2,$3) RETURNING *", [dataInicio, dataFim, porcentagem]);
+		res.status(201).send(descontoInserted.rows)
+	} catch (error) {
+		res.status(500).send({error: "Um erro ocorreu durante essa consulta"})
+	}
+})
+
 module.exports = router;
